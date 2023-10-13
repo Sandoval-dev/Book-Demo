0ff65b3059f7df541c83230c368d0564df4c7a90
Osman Erdem Kılıç <osman_erdem1200@hotmail.com>
2023-10-13 15:47:15 +0300

    Add project files.

---
+++ First_Web_Api/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Service  (Container)
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Run();
